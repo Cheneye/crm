5cf14db3a56d0cc671b3436bf9c4550cf554f506
cl <3328177285@qq.com>
2020-04-09 10:08:00 +0800

    crm实现非法请求拦截功能

---
+++ src/main/java/com/cl/crm/config/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.cl.crm.config;
+
+import com.cl.crm.interceptors.NoLoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public NoLoginInterceptor noLoginInterceptor(){
+        return new NoLoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(noLoginInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/index","/static/**","/user/login");
+    }
+}
